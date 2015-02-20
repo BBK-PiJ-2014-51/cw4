807a8ed6f50348a15a711d31e00f7bfc07b81ceb
caleb <clayton.caleb@gmail.com>
2015-02-20 15:37:38 +0000

    added some test cases for past meeting

---
+++ tests/PastMeetingTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PastMeetingTest {
+
+	@Test
+	public void getSpaceWhenNotesEmpty() {
+		
+	}
+	
+	@Test
+	public void getNotesFromConstructor() {
+		
+	}
+	
+	@Test
+	public void getNotesFromAddMethod() {
+		
+	}
+
+}
