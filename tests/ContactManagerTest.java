7966bcc518757d2956cf797cb396fcb870f873f4
caleb <clayton.caleb@gmail.com>
2015-02-22 18:53:03 +0000

    added more test code for adding and getting meetings

--- tests/ContactManagerTest.java
+++ tests/ContactManagerTest.java
@@ -9,7 +9,9 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
+import impl.ContactManagerImpl;
 import interfaces.Contact;
 import interfaces.ContactManager;
 import interfaces.FutureMeeting;
@@ -21,7 +23,8 @@ import org.junit.Test;
 
 public class ContactManagerTest {
 
-	private static final Calendar TEST_DATE = new GregorianCalendar(2015, Month.JANUARY.getValue(), 1);
+	private static final Calendar PAST_TEST_DATE = new GregorianCalendar(2015, Month.JANUARY.getValue(), 1);
+	private static final Calendar FUTURE_TEST_DATE = new GregorianCalendar(2016, Month.JANUARY.getValue(), 1);
 	private static final String TEST_MEETING_NOTES = "Some test meeting notes";
 	
 	private ContactManager cm = new ContactManagerImpl();
@@ -31,96 +34,122 @@ public class ContactManagerTest {
 	 */
 	
 	@Test
-	public void getMeeting() {
+	public void addGetMeeting_validInput_returnMeeting() {
+		loadTestContacts();
 		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);		
-		Meeting meeting = cm.getMeeting(cm.addFutureMeeting(contactList, TEST_DATE ));
-		boolean equalMeetings =  (meeting.getDate().equals(TEST_DATE) 
+		Meeting meeting = cm.getMeeting(cm.addFutureMeeting(contactList, FUTURE_TEST_DATE ));
+		boolean equalMeetings =  (meeting.getDate().equals(FUTURE_TEST_DATE) 
 				&& meeting.getContacts().equals(contactList) ); 
 		assertEquals(true, equalMeetings);
 	}
 	
+	@Test
+	public void getMeeting_unfoundMeetingParam_ReturnNull() {
+		Meeting meeting = cm.getMeeting(1);
+		assertEquals(null, meeting);
+	}
 	
 	@Test
-	public void getFutureMeeting() {
+	public void addGetFutureMeeting_validInput_returnMeeting() {
+		loadTestContacts();
 		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);		
-		FutureMeeting meeting = cm.getFutureMeeting(cm.addFutureMeeting(contactList, TEST_DATE ));
-		boolean equalMeetings =  (meeting.getDate().equals(TEST_DATE) 
+		FutureMeeting meeting = cm.getFutureMeeting(cm.addFutureMeeting(contactList, FUTURE_TEST_DATE ));
+		boolean equalMeetings =  (meeting.getDate().equals(FUTURE_TEST_DATE) 
 				&& meeting.getContacts().equals(contactList) ); 
 		assertEquals(true, equalMeetings);
 	}
 	
+	@Test (expected = IllegalArgumentException.class)
+	public void addFutureMeeting_PastDateParam_ThrowIllArgEx() {
+		loadTestContacts();
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);		
+		cm.addFutureMeeting(contactList, PAST_TEST_DATE );
+	}
 	
-
-
-	@Test
-	public void getPastMeeting() {
+	@Test (expected = IllegalArgumentException.class)
+	public void addFutureMeeting_UnknownContactParam_ThrowIllArgEx() {
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);		
+		cm.addFutureMeeting(contactList, FUTURE_TEST_DATE );
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void getFutureMeeting_PastMeetingId_ThrowIllArgEx() {
+		loadTestContacts();
 		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);
-		
+		cm.addNewPastMeeting(contactList, PAST_TEST_DATE, TEST_MEETING_NOTES);
+		Object[] contactArray = contactList.toArray();
+		Contact firstContact = (Contact) contactArray[0]; 
+		int meetingId = cm.getPastMeetingList(firstContact).get(0).getId();
+		FutureMeeting meeting = cm.getFutureMeeting(meetingId);
+	}
+	
+	@Test
+	public void getFutureMeeting_unfoundMeetingParam_ReturnNull() {
+		FutureMeeting meeting = cm.getFutureMeeting(1);
+		assertEquals(null, meeting);
+	}
+
+	@Test
+	public void addGetPastMeeting_validInput_returnMeeting() {
+		loadTestContacts();
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);
 		//get the first contact added
 		Object[] contactArray = contactList.toArray();
-		Contact contact = (Contact) contactArray[0]; 
+		Contact firstContact = (Contact) contactArray[0]; 
 		
 		// get the only meeting from contact manager
-		List<PastMeeting> meetings = cm.getPastMeetingList(contact);
-		PastMeeting meeting = null;
-		if (meetings.size() == 1){
-			meetings.get(0); 
-		}
+		cm.addNewPastMeeting(contactList, PAST_TEST_DATE, TEST_MEETING_NOTES);
+		int meetingId = cm.getPastMeetingList(firstContact).get(0).getId();
+		PastMeeting meeting = cm.getPastMeeting(meetingId);
 		
-		boolean equalMeetings =  (meeting.getDate().equals(TEST_DATE) 
+		boolean equalMeetings =  (meeting.getDate().equals(PAST_TEST_DATE) 
 				&& meeting.getContacts().equals(contactList) 
 				&& meeting.getNotes().equals(TEST_MEETING_NOTES)); 
 		assertEquals(true, equalMeetings);
 	}
 	
-	@Test
-	public void addFutureMeetingWithPastDate() {
-		//to do add past meeting as future, expects illegal argument ex
+	@Test (expected = IllegalArgumentException.class)
+	public void addPastMeeting_EmptyContactParam_ThrowIllArgEx() {
+		Set<Contact> contactList = new TreeSet<Contact>();
+		cm.addNewPastMeeting(contactList, PAST_TEST_DATE, TEST_MEETING_NOTES);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void addPastMeeting_UnknownContactParam_ThrowIllArgEx() {
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);
+		cm.addNewPastMeeting(contactList, PAST_TEST_DATE, TEST_MEETING_NOTES);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void addPastMeeting_NullFirstParam_ThrowNullPntrEx() {
+		cm.addNewPastMeeting(null, PAST_TEST_DATE, TEST_MEETING_NOTES);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void addPastMeeting_NullSecondParam_ThrowNullPntrEx() {
+		loadTestContacts();
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);
+		cm.addNewPastMeeting(contactList, null, TEST_MEETING_NOTES);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void addPastMeeting_NullThirdParam_ThrowNullPntrEx() {
+		loadTestContacts();
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);
+		cm.addNewPastMeeting(contactList, PAST_TEST_DATE, null);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void getPastMeeting_FutureIdParam_IllArgEx() {
+		loadTestContacts();
+		Set<Contact> contactList = getContactList(TestContacts.values().length / 2);		
+		PastMeeting meeting = cm.getPastMeeting(cm.addFutureMeeting(contactList, FUTURE_TEST_DATE ));
 	}
 	
 	@Test
-	public void addFutureMeetingWithUnknownContact() {
-		//to do add future meeting with unknown contact, expects illegal argument ex
-	}
-	
-	@Test
-	public void addPastMeetingWithEmptyContacts() {
-		//to do add past meeting with empty set of contacts, expects illegal argument ex
-	}
-	
-	@Test
-	public void addPastMeetingWithUnknownContact() {
-		//to do add past meeting with unknown contact, expects illegal argument ex
-	}
-	
-	@Test
-	public void addPastMeetingWithNullArgument() {
-		//to do add past meeting with null parameter, expects null pointer ex
-	}
-	
-	@Test
-	public void getFutureMeetingWithPastId() {
-		//to do get past meeting by id, expects illegal argument ex
-	}
-	
-	@Test
-	public void getPastMeetingWithFutureId() {
-		//to do get future meeting by id, expects illegal argument ex
-	}
-	
-	@Test
-	public void getNullFromUnfoundMeeting() {
-		//to do get null from unfound id
-	}
-	
-	@Test
-	public void getNullFromUnfoundFutureMeeting() {
-		//to do get null from unfound id
-	}
-	
-	@Test
-	public void getNullFromUnfoundPastMeeting() {
-		//to do get null from unfound id
+	public void getPastMeeting_unfoundMeetingParam_ReturnNull() {
+		PastMeeting meeting = cm.getPastMeeting(1);
+		assertEquals(null, meeting);
 	}
 	
 	/*
@@ -200,7 +229,7 @@ public class ContactManagerTest {
 	}
 	
 	@Test
-	public void getPastMeetingNotesTrhowsIllegalStateIfFuture(){
+	public void getPastMeetingNotesThrowsIllegalStateIfFuture(){
 		//get notes back
 	}
 	
@@ -247,14 +276,19 @@ public class ContactManagerTest {
 		}
 	}
 	
-	
+	/**
+	 * Returns a set of contacts from test data
+	 * 
+	 * @param numContacts number of contacts to return
+	 * @return Set of Contacts
+	 */
 	private Set<Contact> getContactList(int numContacts){
 		Set<Contact> contactList = null;
 		if (numContacts < 0 || numContacts > TestContacts.values().length )
 			return contactList;
 		TestContacts[] testContacts = TestContacts.values();
 		for (int i = 0; i < numContacts; i++){
-			contactList.add(new Contact(testContacts[i].toString(), testContacts[i].getNotes()));
+			contactList.add(new ContactImpl(testContacts[i].toString(), testContacts[i].getNotes()));
 		}
 		
 	}
