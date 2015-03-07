b590eb0cdcd6b35fb663ea82c6d0224ef82afd24
caleb <clayton.caleb@gmail.com>
2015-03-07 15:38:44 +0000

    Added PastMeetingImpl, PastMeetingTest now compiles and fails

---
+++ impl/PastMeetingImpl.java
@@ -0,0 +1,40 @@
+package impl;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import interfaces.Contact;
+import interfaces.PastMeeting;
+
+public class PastMeetingImpl implements PastMeeting {
+
+	public PastMeetingImpl(int tEST_ID, Calendar testDate,
+			Set<Contact> contacts, String testNotes) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- tests/PastMeetingTest.java
+++ tests/PastMeetingTest.java
@@ -2,23 +2,38 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import impl.ContactImpl;
+import impl.PastMeetingImpl;
+import interfaces.Contact;
+import interfaces.PastMeeting;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PastMeetingTest {
-
-	@Test
-	public void getSpaceWhenNotesEmpty() {
-		
-	}
+	
+	private static int TEST_ID = 5;
+	private static final String TEST_NOTES = "test notes";
+	private static final Calendar TEST_DATE = new GregorianCalendar(2015, 06, 15);
+	private Set<Contact> contacts;
+	private PastMeeting pm;
+	
 	
 	@Test
-	public void getNotesFromConstructor() {
-		
+	public void getNotesAfterAddMethod() {
+		int numContacts = 2;
+		contacts = new HashSet<Contact>();
+		for (int i = 0, ch = 'A'; i < numContacts; i++){
+			contacts.add(new ContactImpl(i, "Contact " + ch++, TEST_NOTES + ch));
+		}
+		pm = new PastMeetingImpl(TEST_ID, TEST_DATE, contacts, TEST_NOTES);
+		assertEquals(true, TEST_NOTES.equals(pm.getNotes()));
 	}
 	
-	@Test
-	public void getNotesFromAddMethod() {
-		
-	}
-
 }
