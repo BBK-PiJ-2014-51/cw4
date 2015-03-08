1a06f886c4115cfd0e95829a7d7f9167505a8a46
caleb <clayton.caleb@gmail.com>
2015-03-08 22:24:43 +0000

    implemented several add and get meeting methods

--- impl/ContactManagerImpl.java
+++ impl/ContactManagerImpl.java
@@ -1,8 +1,10 @@
 package impl;
 
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 
 import interfaces.Contact;
 import interfaces.ContactManager;
@@ -11,28 +13,54 @@ import interfaces.Meeting;
 import interfaces.PastMeeting;
 
 public class ContactManagerImpl implements ContactManager {
+	private Set<Contact> contacts = new HashSet<Contact>();
+	private Set<PastMeeting> pastMeetings = new HashSet<PastMeeting>();
+	private Set<FutureMeeting> futureMeetings = new HashSet<FutureMeeting>();
+	private int currentMeetingId = 0;
+	private int currentContactId = 0;
 	
 	@Override
 	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (contacts == null || date == null) throw new NullPointerException("Null parameter!");
+		if (contacts.isEmpty()) throw new IllegalArgumentException("Must supply contacts!");
+		for (Contact contact : contacts){
+			if (!isInDb(contact)) throw new IllegalArgumentException("Unknown Contact!");
+		}
+		futureMeetings.add(new FutureMeetingImpl(currentMeetingId, date, contacts));
+		return currentMeetingId++;
 	}
 
 	@Override
 	public PastMeeting getPastMeeting(int id) {
-		// TODO Auto-generated method stub
+		for (PastMeeting meeting : pastMeetings){
+			if (meeting.getId() == id) return meeting;
+		}
+		for (FutureMeeting meeting : futureMeetings){
+			if (meeting.getId() == id) throw new IllegalArgumentException("Meeting ID represents future meeting!");
+		}
 		return null;
 	}
 
 	@Override
 	public FutureMeeting getFutureMeeting(int id) {
-		// TODO Auto-generated method stub
+		for (FutureMeeting meeting : futureMeetings){
+			if (meeting.getId() == id) return meeting;
+		}
+		for (PastMeeting meeting : pastMeetings){
+			if (meeting.getId() == id)throw new IllegalArgumentException("Meeting ID represents past meeting!");
+		}
 		return null;
 	}
 
 	@Override
 	public Meeting getMeeting(int id) {
-		// TODO Auto-generated method stub
+		for (Meeting meeting : futureMeetings){
+			if (meeting.getId() == id) return meeting;
+		}
+
+		for (Meeting meeting : futureMeetings){
+			if (meeting.getId() == id) return meeting;
+		}
 		return null;
 	}
 
@@ -57,8 +85,19 @@ public class ContactManagerImpl implements ContactManager {
 	@Override
 	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
 			String text) {
-		// TODO Auto-generated method stub
+		if (contacts == null || date == null || text == null) throw new NullPointerException("Null parameter!");
+		if (contacts.isEmpty()) throw new IllegalArgumentException("Must supply contacts!");
+		for (Contact contact : contacts){
+			if (!isInDb(contact)) throw new IllegalArgumentException("Unknown Contact!");
+		}
+		pastMeetings.add(new PastMeetingImpl(currentMeetingId++, date, contacts, text));
+	}
 
+	private boolean isInDb(Contact contact) {
+		for (Contact conFromDb : contacts){
+			if (contact.getId() == conFromDb.getId()) return true;
+		}
+		return false;
 	}
 
 	@Override
@@ -69,8 +108,7 @@ public class ContactManagerImpl implements ContactManager {
 
 	@Override
 	public void addNewContact(String name, String notes) {
-		// TODO Auto-generated method stub
-
+		contacts.add(new ContactImpl(currentContactId++, name, notes));
 	}
 
 	@Override
